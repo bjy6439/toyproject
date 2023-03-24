e088423de6f202b509fa2718cd52a62fba906d20
baegyungmin <bjy6439@naver.com>
2023-03-24 14:05:48 +0900

    Add : 프로젝트 시작

---
+++ src/pages/sub/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../styles/index";
+
+const Subindex = () => {
+  return (
+    <div>
+      <h1>/pages/sub/index.js</h1>
+      <ul>
+        <li>
+          <a href="/">/pages/index.js</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Subindex;
